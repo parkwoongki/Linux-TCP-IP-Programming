422a40e65589194d52defc1ba4ba7a7cf9072d00
parkwoongki <sbe03005@naver.com>
2019-04-17 18:35:57 +0900

    echo-client by using read_n() function

---
+++ 9. echo-client2 by read_n()/echo-client2.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include "rwmod.h"
+
+#define BUF_SIZE 1024
+
+void error_handling(char *message){
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1); 
+}
+
+int main(int argc, char *argv[]){
+	int sockfd, str_len;
+	char message[BUF_SIZE];
+	struct sockaddr_in serv_addr;
+
+	if(argc != 3){
+		printf("Usage : %s <IP> <Port>\n", argv[0]);
+		exit(1);
+	}
+
+	sockfd = socket(PF_INET, SOCK_STREAM, 0); // TCP/IP
+	if(sockfd == -1)
+		error_handling("socket() error");
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
+
+	if(connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+		error_handling("connect() error");
+	else
+		puts("Connected............");
+
+	while(1){
+		fputs("Input message(Q to quit): ",stdout);
+		fgets(message, BUF_SIZE, stdin);
+
+		if(!strcmp(message, "q\n") || !strcmp(message,"Q\n"))
+			break;
+
+		str_len = write(sockfd, message, strlen(message));
+		readn(sockfd, message, str_len);
+		message[str_len]=0;
+
+		printf("Message from server : %s", message);
+	}
+	close(sockfd);
+	return 0;
+}
